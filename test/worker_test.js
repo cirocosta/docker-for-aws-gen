5a29215b592cb083bd7cbad185242f90c75b90d3
Ciro S. Costa <ciro.costa@liferay.com>
2017-05-30 10:31:39 -0300

    Adds launch config copying

--- test/worker_test.js
+++ test/worker_test.js
@@ -8,6 +8,7 @@ const template = require('../templates/17.05.0-ce/edge/template.json');
 const Worker = require('../lib/worker.js');
 
 describe('Worker', () => {
+
   it('Should create copy of ClusterSize', () => {
     let worker = new Worker(template, {
       Name: "Infra",
@@ -17,6 +18,7 @@ describe('Worker', () => {
     assert.isObject(newTempl.Parameters.InfraWorkerSize);
     assert.equal(newTempl.Parameters.InfraWorkerSize.Default, '5');
   });
+
 
   it('Should create copy of InstanceType', () => {
     let worker = new Worker(template, {
@@ -30,12 +32,29 @@ describe('Worker', () => {
     assert.equal(instType.Default, 't2.micro');
   });
 
+
   it('Should create copy of NodeAsg', () => {
     let worker = new Worker(template, {
       Name: "Infra",
     });
 
     const newTempl = worker.create();
-    //  
+    const workerAsg = newTempl.Resources.InfraWorkerAsg
+    assert.isObject(workerAsg);
+    assert.equal(workerAsg.Type, 'AWS::AutoScaling::AutoScalingGroup');
   });
+
+
+  it('Should create copy of NodeLaunchConfig', () => {
+    let worker = new Worker(template, {
+      Name: "Infra",
+    });
+
+    const newTempl = worker.create();
+    const launchConfig = newTempl.Resources[`InfraWorkerLaunchConfig${worker._versionString}`];
+
+    assert.isObject(launchConfig);
+    assert.equal(launchConfig.Type, 'AWS::AutoScaling::LaunchConfiguration');
+  });
+
 });
