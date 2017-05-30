16abba470f734524ce7a14c0cd8bf24023e559f6
Ciro S. Costa <ciro.costa@liferay.com>
2017-05-30 10:57:33 -0300

    Improves documentation

--- diff.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const diff = require('deep-diff').diff;
-
-const lhs = require('./cases/test.original.json') ;
-const rhs = require('./cases/test.json') ;
-
-console.log(diff(lhs,rhs));
-
